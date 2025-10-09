de29ad569e4362f49a1d1aefc929912191886ea3
Qubitium <qubitium@modelcloud.ai>
2025-10-09 15:21:43 +0000

    ruff

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
+
 ROOT_DIR = Path(__file__).resolve().parent
 if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
